8765641a9fce7da8432ac4f2b5f0990f4b0e81f1
Groboclown <matt@groboclown.net>
2014-05-17 13:14:48 -0500

    Started on writing the PHP layer for the film schema.  Updates to the film schema to more easily query for the active quip versions on a film.  This should also allow for easy version history, as well.

---
+++ php/src/WebRiffs/FilmLayer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WebRiffs;
+
+use PBO;
+use Base;
+use Tonic;
+
+/**
+ * Manages the business logic related to the films.
+ */
+class FilmLayer {
+    /**
+     * Creates the film, and adds a new, empty film version.
+     *
+     * @param userId int; must be taken from the user session, so we don't
+     *      check its value.
+     */
+    public static function createFilm($db, $userId, $name, $year,
+            $imdbUrl, $wikipediaUrl) {
+
+
+
+    }
+
+
+    /**
+     * Returns all the films visible by the user.  It includes optional
+     * filters.
+     */
+    public static function findFilms($db, $userId, $sortBy, $rowCount,
+            $startRow = 0, $nameFilter = null, $minYear = 0, $maxYear = 9999) {
+
+    }
+
+
+    public static
+}
